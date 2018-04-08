e1fc15c945eccda7261b24407b9bbe858830bc6b
fengmk2 <fengmk2@gmail.com>
2018-04-08 12:05:35 +0800

    fix: nwjs new disturl

--- config/index.js
+++ config/index.js
@@ -254,7 +254,7 @@ var config = {
       category: 'nwjs',
       enable: true,
       // interval: ms('5m'),
-      disturl: 'https://nwjs.s3-us-west-2.amazonaws.com',
+      disturl: 'https://nwjs2.s3.amazonaws.com',
       description: 'lets you call all Node.js modules directly from DOM and enables a new way of writing applications with all Web technologies.',
       syncerClass: 'nw',
       // for mirrors
