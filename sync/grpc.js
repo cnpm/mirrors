dcf4d03203fa34bb1602cc12fea1787d919d8737
fengmk2 <fengmk2@gmail.com>
2018-08-11 21:44:50 +0800

    fix: sync from new url after grpc@1.14.0 (#197)

--- sync/grpc.js
+++ sync/grpc.js
@@ -34,6 +34,19 @@ class GRPCSyncer extends Sqlite3Syncer {
   // https://storage.googleapis.com/grpc-precompiled-binaries/node/grpc/v1.7.2/node-v57-linux-x64-glibc.tar.gz
   // https://storage.googleapis.com/grpc-precompiled-binaries/node/grpc/v1.7.2/node-v57-win32-x64-unknown.tar.gz
 
+  // >= 1.14.0
+  // https://github.com/grpc/grpc-node/blob/499ddebd1e0126d24e8d58fb096ccf8893e7c2d8/packages/grpc-native-core/package.json#L53
+  // "binary": {
+  //   "module_name": "grpc_node",
+  //   "module_path": "src/node/extension_binary/{node_abi}-{platform}-{arch}-{libc}",
+  //   "host": "https://node-precompiled-binaries.grpc.io/",
+  //   "remote_path": "{name}/v{version}",
+  //   "package_name": "{node_abi}-{platform}-{arch}-{libc}.tar.gz"
+  // },
+  // https://node-precompiled-binaries.grpc.io/grpc/v1.14.0/node-v57-darwin-x64-unknown.tar.gz
+  // https://node-precompiled-binaries.grpc.io/grpc/v1.14.0/node-v57-linux-x64-glibc.tar.gz
+  // https://node-precompiled-binaries.grpc.io/grpc/v1.14.0/node-v64-linux-x64-glibc.tar.gz
+
   formatDownloadItem(fileParent, pkg, nodeAbiVersion, nodePlatform) {
     var glibc = nodePlatform === 'linux' ? 'glibc' : 'unknown';
     var name = `node-${nodeAbiVersion}-${nodePlatform}-x64-${glibc}.tar.gz`;
@@ -41,6 +54,9 @@ class GRPCSyncer extends Sqlite3Syncer {
       name = `node-${nodeAbiVersion}-${nodePlatform}-x64.tar.gz`;
     }
     var downloadURL = `${this._storeUrl}grpc-precompiled-binaries/node/grpc/v${pkg.version}/${name}`;
+    if (semver.satisfies(pkg.version, '>=1.14.0')) {
+      downloadURL = `https://node-precompiled-binaries.grpc.io/grpc/v${pkg.version}/${name}`;
+    }
     return {
       name: name,
       // size: null,
