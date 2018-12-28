1a4fa0cc890020db51f15edcf56d0d97f450b475
alsotang <alsotang@gmail.com>
2018-12-28 19:14:21 +0800

    add test for sync/nw.js

---
+++ test/sync/nw.test.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var assert = require('assert');
+var config = require('../../config');
+var mm = require('mm');
+var NWSyncer = require('../../sync/nw.js');
+
+describe('test/sync/nw.test.js', function () {
+  var syncer = new NWSyncer(config.categories.nwjs);
+
+  describe('listdir()', function () {
+    it('should list versions ', function *() {
+      var items = yield syncer.listdir('/v0.35.0/');
+      assert(items[1].name == 'SHASUMS256.txt')
+    });
+  });
+});
